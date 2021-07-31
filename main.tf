bb2e23179e137df60793dfb1b5aadf219afa75e4
MadhurAsati <madhur9827@gmail.com>
2021-07-31 11:53:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,19 +11,19 @@ locals {
 }
 
 
-data "azurerm_resource_group" "rgrp" {
+data "azurerm_resource_group" "myrgrp" {
   count = var.create_resource_group == false ? 1 : 0
   name  = var.resource_group_name
 }
 
 
-resource "azurerm_resource_group" "rg" {
+resource "azurerm_resource_group" "myrg" {
   count    = var.create_resource_group ? 1 : 0
   name     = var.resource_group_name
   location = var.location
 }
 
-data "azurerm_subnet" "example" {
+data "azurerm_subnet" "snetexample" {
         count                = var.create_vnet_subnet == false ? 1 : 0
         name                 = var.subnet_name 
         virtual_network_name = var.vnet_name 
